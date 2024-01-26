fbb6d1c11c3c124a7e31b63f76935ca2712be821
Nastia Karpova <akkarpova@gmail.com>
2024-01-26 16:03:35 +0100

    Add floating action button and its UI

--- src/app/Components/currentBookshelf.js
+++ src/app/Components/currentBookshelf.js
@@ -13,17 +13,17 @@ export default function CurrentBookshelf({currentBooks, confirmToDelete}) {
                         <div key={currentBook.id} className={styles.cards}>
                             <div className={styles.card}>
                                 
-                                    <div>
-                                        <Image
-                                        src={currentBook.imageLinks && currentBook.imageLinks.thumbnail}
-                                        alt={currentBook.title}
-                                        width={102}
-                                        height={152}
-                                        className={styles["book-image"]}
-                                        />
-                                    </div>
-                                    <div className={styles["book-info"]}>
-
+                                <div>
+                                    <Image
+                                    src={currentBook.imageLinks && currentBook.imageLinks.thumbnail}
+                                    alt={currentBook.title}
+                                    width={102}
+                                    height={152}
+                                    className={styles["book-image"]}
+                                    />
+                                </div>
+                                
+                                <div className={styles["book-info"]}>
                                     <div className={styles["book-title-authors"]}>
                                         <p className={styles["p-title"]}>{currentBook.title}</p>
                                         <p className={styles["p-authors"]}>{currentBook.authors}</p>
@@ -32,12 +32,17 @@ export default function CurrentBookshelf({currentBooks, confirmToDelete}) {
                                         className={styles["btn-delete"]}
                                         onClick={() => confirmToDelete(currentBook.title, "current")}>
                                             Remove
-                                    </button>
+                                    </button>                                    
                                 </div>
                                 
                             </div>
                         </div>
                     ))}
+                    <button 
+                        className={styles["btn-fab"]}
+                            onClick={() => console.log("clicked")}>
+                            +
+                    </button>
             </div>
       </>
     )
