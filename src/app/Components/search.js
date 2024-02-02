ab1e17e8a72a64765fcb8fbcea65d600463fb60c
Nastia Karpova <akkarpova@gmail.com>
2024-02-02 16:49:00 +0100

    Display search results as cards, as to preserve the existing UI

--- src/app/Components/search.js
+++ src/app/Components/search.js
@@ -1,21 +1,20 @@
 "use client"
 
 import { searchBooks } from "../actions";
-// import styles from '../page.module.css';
+import styles from '../page.module.css';
 import { useFormState } from 'react-dom';
 import { useFormStatus } from 'react-dom';
+import Image from 'next/image'
 
 const initialResults = {results: []};
 
 
 export default function Search() {
-
-    console.log(searchBooks);
     
     const [state, formAction] = useFormState(searchBooks, initialResults);
     const { pending } = useFormStatus();
 
-    console.log (state.results);
+
 
     return (
         <>
@@ -28,19 +27,45 @@ export default function Search() {
                     name="query"
                     id="query"
                     placeholder="Search for your next book"
+                    required
                 />
                 <button 
                     type="submit"
                     disabled={pending}>Search</button>
             </form>
 
-            <ul>
+            <div className={styles.currentBookshelf}>
+
                 {state.results && state.results.map(book => (
-                    <li key={book.id}>
-                        {book.title}
-                    </li>
+                    <>
+                    <div key={book.id} className={styles.cards}>
+                        <div className={styles.card}>
+                            <Image
+                                src={book.imageLinks && book.imageLinks.thumbnail}
+                                alt={book.title}
+                                width={102}
+                                height={152}
+                                className={styles["book-image"]}
+                            />
+                        
+                            <div className={styles["book-info"]}>
+                                <div className={styles["book-title-authors"]}>
+                                    <p className={styles["p-title"]}>{book.title}</p>
+                                    <p className={styles["p-authors"]}>{book.authors}</p>
+                                </div>
+                                <button 
+                                    className={styles["btn-delete"]}
+                                    onClick={() => confirmToDelete(book.title, "current")}>
+                                        Remove
+                                </button>                                    
+                            </div>
+                        </div>
+                    </div>
+                </>
+
+                    
                 ))}
-            </ul>
+            </div>
         </>
     )
 }
